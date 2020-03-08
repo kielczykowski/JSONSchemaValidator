36589671811b179b13379f41afbf2cdd0bc6f8a3
Michał Kiełczykowski <michal.k.kielczykowski@gmail.com>
2020-03-08 17:35:31 +0100

    Types added

--- scanner.py
+++ scanner.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import re as regex
 import collections
+import json
 import sys
 
 Token = collections.namedtuple("Token", ["type", "value", "line", "collumn"])
@@ -13,10 +14,13 @@ class Scanner:
     self.current_token_ = 0
     for token in self.tokenize(input_string):
       self.tokens_.append(token)
-    for token in self.tokens_:
-      print(token)
+    with open("TEST.txt",'w') as file:
+      for token in self.tokens_:
+        file.write(str(token))
+        file.write("\n")
 
   def tokenize(self, input_string):
+    types = self.readTypeSpecs()
     keywords = self.readKeywords()
     token_specs = self.readTokenSpecs()
     token_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specs)
@@ -24,17 +28,12 @@ class Scanner:
     line_number = 1
     current_position = line_start = 0
     match = get_token(input_string)
-    # print(input_string)
     print ('token_regex')
     print (token_regex)
     print('keywords')
     print(keywords)
-    # print('token_specs')
-    # print(token_specs)
-    # TODO logic of scanner
     while match is not None:
       type = match.lastgroup
-      # print(match)
       if type == 'NEWLINE':
         line_start = current_position
         line_number += 1
@@ -45,24 +44,18 @@ class Scanner:
           yield Token("QUOT", "\"", line_number, match.start()-line_start)
           if value in keywords:
             type = value.upper()
+          if value in types.keys():
+            type = types[value]
           yield Token(type, value, line_number, match.start()-line_start+1)
           yield Token("QUOT", "\"", line_number, match.end()-(line_start+1))
         else:
           yield Token(type, value, line_number, match.start()-line_start)
-      # print(type)
       current_position = match.end()
       match = get_token(input_string, current_position)
-      # print(current_position)
-      # print(len(input_string))
-      # yield Token(type, "XD", line_number, match.start()-line_start)
     if current_position != len(input_string):
       raise RuntimeError('Error: Unexpected character %r on line %d' % \
                               (input_string[current_position], line_number))
     yield Token('EOF', '', line_number, current_position-line_start)
-
-
-    # TODO return list of tokens
-    # return 0 
 
   def readKeywords(self):
     with open("keywords") as kw:
@@ -71,6 +64,14 @@ class Scanner:
   def readTokenSpecs(self):
     with open("token_specs") as token_specs:
       return [(spec.split("::")[0], spec.split("::")[1].strip(" \t\n")) for spec in token_specs.readlines()]
+  
+  def readTypeSpecs(self):
+    data = {}
+    with open("type_specs") as type_specs:
+      data = json.load(type_specs)
+      return data["types"]
+    # for key, value  in data["types"].items():
+    #   print(key, value)
 
   
   def nextToken(self):
